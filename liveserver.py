270227944db7e0252f66d7f41fcdd495232cf20b
ViniMSLima <vinisary.lima@gmail.com>
2024-09-18 10:18:31 -0300

    kjfdhgkjdfbhskghbsdrakgibare kigharebgi habe grkj

--- liveserver.py
+++ liveserver.py
@@ -6,14 +6,11 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @socketio.on('video_frame')
 def handle_video_frame(data):
-    # Retransmitir o frame para todos os clientes conectados
     emit('video_frame', data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+
+
